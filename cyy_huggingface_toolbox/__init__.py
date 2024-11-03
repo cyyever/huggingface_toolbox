ea17ea9006f63cd2a242e296ca2f984d277bf5f5
cyy <cyyever@outlook.com>
2024-11-03 13:08:52 +0800

    Add tokenizer

--- cyy_huggingface_toolbox/__init__.py
+++ cyy_huggingface_toolbox/__init__.py
@@ -1,3 +1,17 @@
-from .data_pipeline import *
-from .dataset import *
-from .model import *
+from .data_pipeline import squeeze_huggingface_input
+from .dataset import HunggingFaceFactory
+from .model import (
+    get_huggingface_constructor,
+    get_model_evaluator,
+    HuggingFaceModelEvaluator,
+)
+from .tokenizer import HuggingFaceTokenizer
+
+__all__ = [
+    "squeeze_huggingface_input",
+    "HunggingFaceFactory",
+    "HuggingFaceModelEvaluator",
+    "get_huggingface_constructor",
+    "get_model_evaluator",
+    "HuggingFaceTokenizer",
+]
