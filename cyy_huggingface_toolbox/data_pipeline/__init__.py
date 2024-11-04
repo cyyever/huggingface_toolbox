e6710b6ae9299eb2ec3765fa5866eb6c00247ee6
cyy <cyyever@outlook.com>
2024-11-05 02:24:40 +0800

    Fix code

--- cyy_huggingface_toolbox/data_pipeline/__init__.py
+++ cyy_huggingface_toolbox/data_pipeline/__init__.py
@@ -1,6 +1,9 @@
+import torch
+
+
 def squeeze_huggingface_input(huggingface_input: dict) -> dict:
-    for k in ("input_ids", "attention_mask"):
-        if k in huggingface_input:
+    for k, v in huggingface_input.items():
+        if isinstance(v, torch.Tensor):
             huggingface_input[k] = huggingface_input[k].squeeze(dim=0)
     return huggingface_input
 
--- cyy_huggingface_toolbox/data_pipeline/transform.py
+++
@@ -1,5 +0,0 @@
-def squeeze_huggingface_input(huggingface_input) -> None:
-    for k in ("input_ids", "attention_mask"):
-        if k in huggingface_input:
-            huggingface_input[k] = huggingface_input[k].squeeze(dim=0)
-    return huggingface_input
