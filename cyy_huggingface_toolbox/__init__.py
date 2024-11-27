87cdb8d87490b0bda865324ad40df8e9fa42b0ee
cyy <cyyever@outlook.com>
2024-11-27 20:28:33 +0800

    Fix code

--- cyy_huggingface_toolbox/__init__.py
+++ cyy_huggingface_toolbox/__init__.py
@@ -1,8 +1,13 @@
 from .model import (
     HuggingFaceModelEvaluator,
+    HuggingFaceModelEvaluatorForFinetune,
 )
 from .tokenizer import HuggingFaceTokenizer
 from .dataset import *
 from .data_pipeline import *
 
-__all__ = ["HuggingFaceModelEvaluator", "HuggingFaceTokenizer"]
+__all__ = [
+    "HuggingFaceModelEvaluator",
+    "HuggingFaceTokenizer",
+    "HuggingFaceModelEvaluatorForFinetune",
+]
