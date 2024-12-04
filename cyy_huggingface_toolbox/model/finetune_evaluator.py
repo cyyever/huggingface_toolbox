b56ddba76a7e64cd910f79250799a35774267e0b
cyy <cyyever@outlook.com>
2024-12-04 12:58:43 +0800

    Fix code

--- cyy_huggingface_toolbox/model/finetune_evaluator.py
+++ cyy_huggingface_toolbox/model/finetune_evaluator.py
@@ -1,11 +1,15 @@
 from typing import Any
 
+import torch.nn
 from cyy_torch_toolbox import ModelType
 from peft import LoraConfig
 from peft.mapping import get_peft_model
 from peft.peft_model import PeftModel
 from peft.utils.peft_types import TaskType
-from transformers import PreTrainedModel
+from peft.utils.save_and_load import (
+    get_peft_model_state_dict,
+    set_peft_model_state_dict,
+)
 
 from .evaluator import HuggingFaceModelEvaluator
 
@@ -13,8 +17,6 @@ from .evaluator import HuggingFaceModelEvaluator
 class HuggingFaceModelEvaluatorForFinetune(HuggingFaceModelEvaluator):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        model = self.model
-        assert isinstance(model, PreTrainedModel)
         assert self.model_type == ModelType.CausalLM
         peft_config = LoraConfig(
             target_modules=[
@@ -28,8 +30,17 @@ class HuggingFaceModelEvaluatorForFinetune(HuggingFaceModelEvaluator):
             ],
             task_type=TaskType.CAUSAL_LM,
         )
-        self.set_model(get_peft_model(model=model, peft_config=peft_config))
+        self.set_model(get_peft_model(model=self.model, peft_config=peft_config))
 
-    @property
-    def model(self) -> PeftModel:
-        return super().model
+    def peft_model(self) -> PeftModel:
+        _model = self.model
+        assert isinstance(_model, PeftModel)
+        return _model
+
+    def load_model(self, model: torch.nn.Module) -> None:
+        if self.model is model:
+            return
+        set_peft_model_state_dict(
+            model=self.peft_model,
+            peft_model_state_dict=get_peft_model_state_dict(model=model),
+        )
