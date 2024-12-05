d601c883ecfc29179a8b291e30b3716b4e5bb2c1
cyy <cyyever@outlook.com>
2024-12-05 14:03:25 +0800

    Fix code

--- cyy_huggingface_toolbox/model/finetune_evaluator.py
+++ cyy_huggingface_toolbox/model/finetune_evaluator.py
@@ -1,18 +1,19 @@
+import copy
 from typing import Any
 
 import torch.nn
 from cyy_naive_lib.fs.tempdir import TempDir
-from cyy_torch_toolbox import ModelType
+from cyy_torch_toolbox import ModelType, TensorDict
 from peft.auto import AutoPeftModelForCausalLM
 from peft.mapping import get_peft_model
 from peft.peft_model import PeftModel
 from peft.tuners.lora.config import LoraConfig
 from peft.utils.peft_types import TaskType
+from peft.utils.save_and_load import (
+    get_peft_model_state_dict,
+    set_peft_model_state_dict,
+)
 
-# from peft.utils.save_and_load import (
-#     get_peft_model_state_dict,
-#     set_peft_model_state_dict,
-# )
 from .evaluator import HuggingFaceModelEvaluator
 
 
@@ -31,19 +32,21 @@ class HuggingFaceModelEvaluatorForFinetune(HuggingFaceModelEvaluator):
         assert isinstance(_model, PeftModel)
         return _model
 
-    def load_model_for_inference(self, model: torch.nn.Module) -> None:
+    @classmethod
+    def get_perf_model_state_dict(cls, model: torch.nn.Module) -> TensorDict:
+        assert isinstance(model, PeftModel)
         with TempDir() as dir_path:
-            self.model.save_pretrained(dir_path)
+            model.save_pretrained(dir_path)
             model = AutoPeftModelForCausalLM.from_pretrained(dir_path)
-            self.set_model(model)
+            return get_peft_model_state_dict(model=model)
 
-        # assert isinstance(model, PeftModel)
-        # print(model)
-        # model.dequantize()
-        # print(model)
-        # if self.model is model:
-        #     return
-        # set_peft_model_state_dict(
-        #     model=self.peft_model,
-        #     peft_model_state_dict=get_peft_model_state_dict(model=model),
-        # )
+    def load_model_for_inference(self, model: torch.nn.Module) -> None:
+        if self.model is model:
+            self.set_model(copy.copy(model))
+
+        set_peft_model_state_dict(
+            model=self.peft_model,
+            peft_model_state_dict=HuggingFaceModelEvaluatorForFinetune.get_perf_model_state_dict(
+                model
+            ),
+        )
