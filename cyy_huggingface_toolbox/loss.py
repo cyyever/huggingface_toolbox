303c3f312eb3a1c43bdec2d5bf7a4a219d285c85
cyy <cyyever@outlook.com>
2025-03-15 18:25:25 +0800

    Fix code

--- cyy_huggingface_toolbox/loss.py
+++ cyy_huggingface_toolbox/loss.py
@@ -1,5 +1,66 @@
 from typing import Any
 import torch.nn
+import torch.nn.functional as F
+
+
+# Copyied from torchvision
+def sigmoid_focal_loss(
+    inputs: torch.Tensor,
+    targets: torch.Tensor,
+    alpha: float = 0.25,
+    gamma: float = 2,
+    ignore_index: int = -100,
+    reduction: str = "none",
+) -> torch.Tensor:
+    """
+    Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+
+    Args:
+        inputs (Tensor): A float tensor of arbitrary shape.
+                The predictions for each example.
+        targets (Tensor): A float tensor with the same shape as inputs. Stores the binary
+                classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+        alpha (float): Weighting factor in range (0,1) to balance
+                positive vs negative examples or -1 for ignore. Default: ``0.25``.
+        gamma (float): Exponent of the modulating factor (1 - p_t) to
+                balance easy vs hard examples. Default: ``2``.
+        reduction (string): ``'none'`` | ``'mean'`` | ``'sum'``
+                ``'none'``: No reduction will be applied to the output.
+                ``'mean'``: The output will be averaged.
+                ``'sum'``: The output will be summed. Default: ``'none'``.
+    Returns:
+        Loss tensor with the reduction option applied.
+    """
+    # Original implementation from https://github.com/facebookresearch/fvcore/blob/master/fvcore/nn/focal_loss.py
+
+    # inputs=
+    p = torch.sigmoid(inputs)
+    ce_loss = torch.nn.functional.cross_entropy(
+        inputs, targets, reduction="none", ignore_index=ignore_index
+    )
+    p_t = p * targets + (1 - p) * (1 - targets)
+    loss = ce_loss * ((1 - p_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        loss = alpha_t * loss
+    assert loss.shape[0] == targets.shape[0]
+    mask = targets != ignore_index
+    loss = loss[mask]
+
+    # Check reduction option and return loss accordingly
+    if reduction == "none":
+        pass
+    elif reduction == "mean":
+        loss = loss.mean()
+    elif reduction == "sum":
+        loss = loss.sum()
+    else:
+        raise ValueError(
+            f"Invalid Value for arg 'reduction': '{reduction} \n Supported reduction modes: 'none', 'mean', 'sum'"
+        )
+    return loss
 
 
 # copied from HuggingFace ForCausalLMLoss
@@ -24,8 +85,11 @@ def focal_loss(
     # Enable model parallelism
     shift_labels = shift_labels.to(logits.device)
     reduction = "sum" if num_items_in_batch is not None else "mean"
-    loss = torch.nn.functional.cross_entropy(
-        logits, shift_labels, ignore_index=ignore_index, reduction=reduction
+    loss = sigmoid_focal_loss(
+        inputs=logits,
+        targets=shift_labels,
+        ignore_index=ignore_index,
+        reduction=reduction,
     )
     if reduction == "sum":
         loss = loss / num_items_in_batch
