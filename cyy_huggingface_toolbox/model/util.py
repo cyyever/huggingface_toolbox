18cd3b5fb15a6ae463e0db97d657efc830d4a8c8
cyy <cyyever@outlook.com>
2025-08-10 12:21:56 +0800

    Fix code

--- cyy_huggingface_toolbox/model/util.py
+++ cyy_huggingface_toolbox/model/util.py
@@ -2,6 +2,7 @@ import os
 
 from peft.peft_model import PeftModel
 from transformers import AutoModelForCausalLM
+from vllm import LLM
 
 
 def merge_peft_model_for_vllm(
@@ -17,3 +18,21 @@ def merge_peft_model_for_vllm(
     assert not os.path.exists(saved_model_path)
     model.save_pretrained(saved_model_path)
     return saved_model_path
+
+
+def get_vllm(
+    pretrained_model_name_or_path: str, finetuned_model_dir: str | None = None
+) -> LLM:
+    model_name = pretrained_model_name_or_path
+    if finetuned_model_dir is not None:
+        pretrained_model_name_or_path = merge_peft_model_for_vllm(
+            pretrained_model_name_or_path, finetuned_model_dir
+        )
+    llm = LLM(
+        model=pretrained_model_name_or_path,
+        generation_config="auto",
+        tokenizer=model_name,
+        dtype="bfloat16",
+        max_model_len=2048,
+    )
+    return llm
