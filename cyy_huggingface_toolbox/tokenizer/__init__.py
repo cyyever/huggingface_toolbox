88f243e7fa49b38ef78d7fd10f1b48444c2bbc17
cyy <cyyever@outlook.com>
2025-08-23 11:52:11 +0800

    Fix code

--- cyy_huggingface_toolbox/tokenizer/__init__.py
+++ cyy_huggingface_toolbox/tokenizer/__init__.py
@@ -7,19 +7,21 @@ from cyy_torch_toolbox import TokenIDsType, TokenIDType, TokenizerMixin
 
 class HuggingFaceTokenizer(TokenizerMixin):
     def __init__(self, tokenizer_config: dict) -> None:
-        self.__tokenizer: transformers.PreTrainedTokenizerFast = (
-            transformers.AutoTokenizer.from_pretrained(
-                tokenizer_config["name"],
-                use_fast=True,
-                **tokenizer_config.get("kwargs", {}),
-            )
-        )
-        if self.tokenizer.pad_token is None:
-            self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer_config = tokenizer_config.copy()
         # # # self.tokenizer.padding_side = "left"
 
     @property
     def tokenizer(self) -> transformers.PreTrainedTokenizerFast:
+        if self.__tokenizer is None:
+            self.__tokenizer: transformers.PreTrainedTokenizerFast = (
+                transformers.AutoTokenizer.from_pretrained(
+                    self.tokenizer_config["name"],
+                    use_fast=True,
+                    **self.tokenizer_config.get("kwargs", {}),
+                )
+            )
+            if self.tokenizer.pad_token is None:
+                self.tokenizer.pad_token = self.tokenizer.eos_token
         return self.__tokenizer
 
     def __get_batch_encoding_from_transformed_result(
